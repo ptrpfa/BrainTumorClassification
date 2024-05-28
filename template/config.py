d0b00716d7983b6d4fea87814f349e3d1d3363ec
P <30462819+ptrpfa@users.noreply.github.com>
2024-05-28 18:53:38 +0800

    completed preliminary mobilenetv3 model

---
+++ template/config.py
@@ -0,0 +1,10 @@
+""" Configurations """
+# Folders
+EXPORT_FOLDER = "../export"
+MAIN_DATASET_FOLDER = "../dataset"
+DATASET_FOLDER = f"{MAIN_DATASET_FOLDER}/dataset_1"
+MODEL_FOLDER = "./models"
+
+# Parameters
+TEST_SIZE = 0.3
+RANDOM_STATE = 123456
