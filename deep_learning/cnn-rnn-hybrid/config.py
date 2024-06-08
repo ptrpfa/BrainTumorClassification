ec733ac556e6dc7963f83d43eeaac26f78191af2
lionel-sim <lionelsimweixian@gmail.com>
2024-06-08 22:27:13 +0800

    my dream job is to be a data scientist

---
+++ deep_learning/cnn-rnn-hybrid/config.py
@@ -0,0 +1,15 @@
+""" Configurations """
+# Folders
+EXPORT_FOLDER = "../../export"
+MAIN_DATASET_FOLDER = "../../dataset"
+DATASET_FOLDER = f"{MAIN_DATASET_FOLDER}/dataset_1"
+TRAIN_FOLDER = "../../dataset/train_test_dataset/train"
+TEST_FOLDER = "../../dataset/train_test_dataset/test"
+MODEL_FOLDER = "./models"
+
+# Parameters
+TEST_SIZE = 0.3
+RANDOM_STATE = 123456
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
