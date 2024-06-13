acfacf2f9d0066f3526aceccf1bf4ac6f8675e12
P <30462819+ptrpfa@users.noreply.github.com>
2024-06-14 00:38:53 +0800

    added xception model, need GPU

---
+++ transfer_learning/xception/config.py
@@ -0,0 +1,13 @@
+""" Configurations """
+# Folders
+EXPORT_FOLDER = "../../export"
+MAIN_DATASET_FOLDER = "../../dataset"
+DATASET_FOLDER = f"{MAIN_DATASET_FOLDER}/dataset_1"
+MODEL_FOLDER = "./models"
+
+# Files
+MODEL_FILE = f"{MODEL_FOLDER}/xception_model.keras"
+
+# Parameters
+TEST_SIZE = 0.3
+RANDOM_STATE = 123456
